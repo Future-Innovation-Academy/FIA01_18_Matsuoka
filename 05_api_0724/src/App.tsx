918440b9e5a5b0156c1ad1a934f31487ccbf2062
18matsuoka <106858578+18matsuoka@users.noreply.github.com>
2022-07-30 17:18:21 +0900

    Add files via upload

--- 05_api_0724/src/App.tsx
+++ 05_api_0724/src/App.tsx
@@ -13,9 +13,46 @@ import {
   QuerySnapshot,
 } from "firebase/firestore";
 import { db, auth } from "./firebase"; //.envに書かれているfirebaseに接続するためのもの
-
+import { onAuthStateChanged } from "firebase/auth";
+import { signInWithRedirect } from "firebase/auth";
+import { getRedirectResult, GoogleAuthProvider } from "firebase/auth";
 
 function App() {
+
+  //認証ロジック
+  const provider = new GoogleAuthProvider();
+  const clickLogin = function () {
+    signInWithRedirect(auth, provider);
+  }
+  useEffect(() => {
+    getRedirectResult(auth)
+      .then((result) => {
+        console.log(result);
+        if (result !== null) {
+          const credential = GoogleAuthProvider.credentialFromResult(result);
+          const token = credential?.accessToken;
+          // console.log(token,"token");
+          // The signed-in user info.
+          const user = result.user;
+          // console.log(user,"user");
+          // console.log(user.uid,"userid");
+          // updateDoc(docRef, { user: [...data[0].user,user.photoURL] });
+          setYou(String(user.photoURL));
+        }
+      }).catch((error) => {
+        console.error(error);
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        const email = error.email;
+        // console.error(errorCode);
+        // console.error(errorMessage);
+        // console.error(email);
+        // The AuthCredential type that was used.
+        //const credential = GoogleAuthProvider.credentialFromError(error);
+      });
+  }, []);
+
 
   //トランプの型
   type cardType = {
@@ -23,23 +60,27 @@ function App() {
     images: { png: string, svg: string },
     value: string,
     suit: string,
-    code: string
+    code: string,
   }
 
   //コレクションの型
   type collectionData = {
     gameFlag: boolean,
     sumNum: number,
+    preSumNum:number,
     deck_id: string,
     card: cardType,
     preCardList: cardType[],
     cardList: cardType[],
+    user:string,
+    preUser:string,
   }
 
   const [data, setData] = useState<collectionData[]>([
     {
       gameFlag: false,
       sumNum: 0,
+      preSumNum:0,
       deck_id: "",
       card: {
         image: "",
@@ -49,12 +90,20 @@ function App() {
         code: ""
       },
       preCardList: [],
-      cardList: []
+      cardList: [],
+      user:"",
+      preUser:"",
     }],
   );
 
   //モーダルの開閉用
   const [open, setOpen] = useState<boolean>(false);
+
+  //r－るのモーダル開閉用
+  const [openRule, setOpenRule] = useState<boolean>(false);
+
+  //現在の認証者
+  const [you, setYou] = useState<string|undefined>(undefined);
 
   //コレクションの情報取得
   const q = collection(db, "kadai_20220731");
@@ -63,9 +112,6 @@ function App() {
   //ゲーム終了時処理
   const gameClose = (): void => {
     setOpen(false)
-
-    //ページのリロード
-    window.location.reload()
   }
 
   //トランプの数の総和が特定の値を超えたらゲーム終了のモーダルをオープンする。
@@ -79,7 +125,6 @@ function App() {
         updateDoc(docRef, { preCardList: [] });
         updateDoc(docRef, { cardList: [] });
         updateDoc(docRef, { deck_id: "" });
-        updateDoc(docRef, { sumNum: 0 });
         updateDoc(docRef, {
           card: {
             image: "",
@@ -93,21 +138,22 @@ function App() {
     }, [data[0].sumNum]
   )
 
-  //コレクションの値を同期
+  //コレクションの値を画面と同期
   useEffect(() => {
-    //2.1 query=コレクション(firebaseのデータが入る箱のこと)
-    const q = query(collection(db, "kadai_20220731")); //データにアクセス
+    const q = query(collection(db, "kadai_20220731"));
 
-    // 2.2
     const unsub = onSnapshot(q, (QuerySnapshot) => {
       setData(
         QuerySnapshot.docs.map((doc) => ({
           gameFlag: doc.data().gameFlag,
           sumNum: doc.data().sumNum,
+          preSumNum:doc.data().preSumNum,
           deck_id: doc.data().deck_id,
           card: doc.data().card,
           preCardList: doc.data().preCardList,
           cardList: doc.data().cardList,
+          user:doc.data().user,
+          preUser:doc.data().preUser,
         }))
       );
     });
@@ -117,13 +163,18 @@ function App() {
 
   const gameStart = async () => {
 
+    //スコアはゲーム開始と同時に初期化
+    updateDoc(docRef, { sumNum: 0 });
+    updateDoc(docRef, { preSumNum: 0 });
+
+
+
     //トランプのシャッフル
     const response = await fetch(
       "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1"
     ).then((res) => res.json())
       .then((data) => {
         updateDoc(docRef, { deck_id: data.deck_id });
-        // setdeckID(data.deck_id);
       });
 
     //ゲーム開始フラグをtrueにする。
@@ -131,7 +182,6 @@ function App() {
   }
 
   const hitCard = async () => {
-
     //カードを一枚引く＾
     const response = await fetch(
       `https://deckofcardsapi.com/api/deck/${data[0].deck_id}/draw/?count=1`
@@ -143,6 +193,19 @@ function App() {
 
         updateDoc(docRef, { cardList: [...data[0].cardList, cdata.cards[0]] });
 
+        //一つ前にカードを引いたユーザ情報を格納する。認証が無い場合は名無しを入れる。
+        if(data[0].user !== undefined){
+          updateDoc(docRef, { preUser: data[0].user });
+        } else {
+          updateDoc(docRef, { preUser:"nanashi" });
+        }
+
+        //今カードを引いたユーザの情報を格納する。認証が無い場合は名無しを入れる。
+        if(you !== undefined){
+          updateDoc(docRef, { user: you });
+        } else {
+          updateDoc(docRef, { user: "nanashi" });
+        }
 
         //レスポンスの数値変換をする
         let tmpNumber: number = 0;
@@ -162,14 +225,15 @@ function App() {
           default:
             tmpNumber = Number(cdata.cards[0].value);
         }
-        updateDoc(docRef, { sumNum: (data[0].sumNum + tmpNumber)});
+        updateDoc(docRef, { preSumNum: (data[0].sumNum) });
+        updateDoc(docRef, { sumNum: (data[0].sumNum + tmpNumber) });
       });
   }
 
   return (
     <div className="App">
 
-      <h1>Don't exceed 100！</h1>
+      <h2>Don't exceed 100！</h2>
 
       {(data[0].gameFlag === false) ? "" : <p>今引いたカード</p>}
       {/* 直前に引いたカード表示 */}
@@ -194,6 +258,10 @@ function App() {
           //ゲーム開始後の表示
           : <Button variant="contained" color="success" onClick={hitCard}>Hit Card</Button>
       }
+      　<Button className="rule" variant="contained" onClick={()=>setOpenRule(true)}>Rule</Button>
+      <div>
+        <button className="authentication" onClick={() => clickLogin()}>Google authentication</button>
+      </div>
 
       <Modal
         open={open}
@@ -205,19 +273,71 @@ function App() {
             top: '50%',
             position: 'absolute',
             maxWidth: '100%',
-            minWidth: '400px',
+            minWidth: '500px',
             maxHeight: '70%',
             transform: 'translate(-50%, -50%)',
           }}
         >
           <Box textAlign="center">
-            <DialogTitle>You Lose...</DialogTitle>
+            <DialogTitle>
+            {(data[0].gameFlag === true)&&("Please close the modal to go to next game...")}
+            {(data[0].gameFlag === false)&&("Game finished!")}
+
+
+              </DialogTitle>
             <DialogContent>
-              <DialogContentText>Please close the modal to restart the game</DialogContentText>
+              <DialogContentText>
+                {/* 次のゲームが始まっている場合は下記を表示する */}
+                {(data[0].gameFlag === true)?"Next game has already started...":"" }
+
+                {/* 次のゲームが始まっていない場合にスコア結果を表示する。 */}
+                {(data[0].gameFlag === false)&&
+                // 認証していないメンバの場合は名無しでそうでない場合は認証者のアイコン表示
+                (<span>Winner：{((data[0].preUser === "nanashi")?"名無し":
+                <img src={data[0].preUser} style={{height:'30px',width:'30px'}}/>)} {data[0].preSumNum}Points!</span>)}
+                </DialogContentText>
+                
+                <DialogContentText>
+                {/* 次のゲームが始まっていない場合にスコア結果を表示する。 */}
+                {(data[0].gameFlag === false)&&
+                // 認証していないメンバの場合は名無しでそうでない場合は認証者のアイコン表示
+                (<span>Loser  ：{((data[0].user === "nanashi")?"名無し":
+                <img src={data[0].user} style={{height:'30px',width:'30px'}} />)} {data[0].sumNum}Points!</span>)}
+                </DialogContentText>
             </DialogContent>
           </Box>
         </Paper>
       </Modal>
+
+      {/* ルール表示モーダル */}
+      <Modal
+        open={openRule}
+        onClose={() => setOpenRule(false)}
+      >
+        <Paper
+          style={{
+            left: '50%',
+            top: '50%',
+            position: 'absolute',
+            maxWidth: '100%',
+            minWidth: '800px',
+            maxHeight: '70%',
+            transform: 'translate(-50%, -50%)',
+          }}
+        >
+          <Box textAlign="center">
+            <DialogTitle>■ルール説明</DialogTitle>
+            <DialogContent>
+            <DialogContentText>・トランプを次々に引き、カードの値の総数で100を超えてしまった人は負け！</DialogContentText>
+            <DialogContentText>・100を超える一つ直前にカードを引いていた人が勝ち！</DialogContentText>
+            {/* 下記は改行用 */}
+            <DialogContentText>　</DialogContentText>
+            <DialogContentText>※Google認証をするとゲーム終了時に勝者・敗者のアイコンが表示されます。</DialogContentText>
+            </DialogContent>
+          </Box>
+        </Paper>
+      </Modal>
+
 
     </div >
   )
